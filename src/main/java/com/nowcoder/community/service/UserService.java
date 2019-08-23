410203474e2c84c0b07b2ed4f11171935af27008
emyu <1416906645@qq.com>
2019-08-23 15:27:48 +0800

    首次提交，登录注册功能完成。

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -1,17 +1,190 @@
 package com.nowcoder.community.service;
 
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
     @Autowired
     private UserMapper userMapper;
 
+    //邮件发送所需 对象
+    @Autowired
+    private MailClient mailClient;
+
+    //模板引擎所需 对象
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    LoginTicketMapper loginTicketMapper;
+
+    //拼接激活链接 所需要的部分 写在配置文件中 这样代码就不用修改
+    @Value("${community.path.domain}")
+    private String domain;
+
+    //拼接激活链接 所需要的部分
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User findUserById(int id){
         return userMapper.selectById(id);
+    }
+
+    //给LoginController来调用的
+    //参数：User对象
+    //返回值：一个map 包含（user对象的属性为空、已存在和注册完成等情况）
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        //先对传进来的参数 User 进行判断 输入合不合法
+        //用map 封装 判断 处理的结果
+        if (user == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        if (StringUtils.isBlank(user.getUsername())){
+            map.put("usernameMsg","账号不能为空！");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())){
+            map.put("passwordMsg","密码不能为空！");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())){
+            map.put("emailMsg","邮箱不能为空！");
+            return map;
+        }
+
+        //验证账号 去数据库查 是否已经被注册
+        User u = userMapper.selectByName(user.getUsername());
+        if(u != null) {
+            map.put("usernameMsg", "该账号已存在！");
+            return map;
+        }
+
+        //验证邮箱
+        u = userMapper.selectByEmail(user.getEmail());
+        if(u != null) {
+            map.put("emailMsg","该邮箱已被注册！");
+            return map;
+        }
+
+        //注册用户
+        //先设置user各个属性的值
+        //user的盐 再对密码加盐
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        user.setType(0);
+        //未激活的状态为0
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        //利用java自带工具组件 生成随机头像
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        //user属性完善后 存入到数据库里面
+        userMapper.insertUser(user);
+
+
+        //发送激活邮件
+        //往模板activation.html里面填数据
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // http://localhost:8080/community/activation/101/code
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url",url);
+        String content = templateEngine.process("/mail/activation", context);
+
+        //发送邮件
+        mailClient.sendMail(user.getEmail(),"注册账号激活",content );
+
+        return map;
+    }
+
+
+    //激活的service
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if(user.getActivationCode().equals(code)) {
+            //激活了状态就为1
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
+    }
+
+    public Map<String, Object> login(String username, String password, long expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+
+        //空值处理
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空");
+            return map;
+        }
+
+        //验证账号
+        User user = userMapper.selectByName(username);
+        if(user == null) {
+            map.put("usernameMsg", "该账号不存在！");
+            return map;
+        }
+
+        //验证状态
+        if(user.getStatus() == 0) {
+            map.put("usernameMsg", "该账号未激活！");
+            return map;
+        }
+
+        //验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if(!user.getPassword().equals(password)) {
+            map.put("passwordMsg", "密码不正确！");
+            return map;
+        }
+
+        //生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000 ));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+
+
+
+        //登录都没有问题  就返回带有ticket的map
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
+
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket, 1);
+    }
+
+    public LoginTicket findLoginTicket (String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
     }
 }
